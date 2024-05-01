788acb0fd7a3edbeab05d3a1ae54bdc3e614877b
rubisanto <rubisanto06@gmail.com>
2024-05-01 12:55:06 +0200

    Implementation point 1 counter

--- src/views/Home.js
+++ src/views/Home.js
@@ -1,16 +1,11 @@
-import React from 'react';
+import React from "react";
 
-function Home({
-  onIncrement,
-  counter,
-}) {
+function Home({ onIncrement, counter }) {
   return (
     <div>
-      <button>
-        Vous avez cliqué {counter} fois
-      </button>
+      <button onClick={onIncrement}>Vous avez cliqué {counter} fois</button>
     </div>
   );
-};
+}
 
 export default Home;
